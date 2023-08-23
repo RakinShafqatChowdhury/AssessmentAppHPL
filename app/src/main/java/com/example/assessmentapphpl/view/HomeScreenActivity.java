7c54945bf498f3e55e2e11e6f9fc9e9ea41a4844
rakin-cel <rakin.cel@gmail.com>
2023-08-23 12:10:56 +0600

    user details shown on homescreen

--- app/src/main/java/com/example/assessmentapphpl/view/HomeScreenActivity.java
+++ app/src/main/java/com/example/assessmentapphpl/view/HomeScreenActivity.java
@@ -1,21 +1,39 @@
 package com.example.assessmentapphpl.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.assessmentapphpl.R;
+import com.example.assessmentapphpl.databinding.ActivityHomeScreenBinding;
 import com.example.assessmentapphpl.helper.SharedPref;
 import com.example.assessmentapphpl.helper.Utils;
+import com.example.assessmentapphpl.model.UserRegistrationModel;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
+    private ActivityHomeScreenBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home_screen);
 
-        Utils.alert(this, SharedPref.getInstance(this).getUserInfo().getUserName(),
-                SharedPref.getInstance(this).getUserInfo().getUserEmail()+" "+SharedPref.getInstance(this).getUserInfo().getUserPhoneNumber());
+        UserRegistrationModel userRegistrationModel = SharedPref.getInstance(this).getUserInfo();
+
+        binding.setUserInfoModel(userRegistrationModel);
+
+        binding.createNewBtn.setOnClickListener(view -> gotoTaskEntryPage());
+        binding.historyBtn.setOnClickListener(view -> gotoTaskDataPage());
+
+    }
+
+    private void gotoTaskDataPage() {
+
+    }
+
+    private void gotoTaskEntryPage() {
+
     }
 }
